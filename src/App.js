fdd8ed4bbb33749e7739a1271d5815001fd5f0ba
Ausha Richardson <ausha.richardson@gmail.com>
2019-10-10 12:27:22 -0400

    update

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello Ausha.
+          Hello, my good friend Ausha.
         </p>
         <a
           className="App-link"
